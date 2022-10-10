4edeb00a7c250542cbc951df78332ac53a5f5d11
josephatJ <josephatjulius24@gmail.com>
2022-10-10 12:42:49 +0300

    feat: add support to send message and save payload to datastore

--- src/app/pages/user-accounts/components/request-user-accounts/request-user-accounts.component.ts
+++ src/app/pages/user-accounts/components/request-user-accounts/request-user-accounts.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DataStoreDataService } from 'src/app/core/services/datastore.service';
+import { MessagesAndDatastoreService } from 'src/app/core/services/messages-and-datastore.service';
 import { Dropdown } from 'src/app/shared/modules/form/models/dropdown.model';
 import { Email } from 'src/app/shared/modules/form/models/email.model';
 import { Field } from 'src/app/shared/modules/form/models/field.model';
@@ -14,6 +17,7 @@ import { Textbox } from 'src/app/shared/modules/form/models/text-box.model';
 export class RequestUserAccountsComponent implements OnInit {
   @Input() configurations: any;
   @Input() currentUser: any;
+  @Input() systemConfigs: any;
   formFields: Field<string>[];
   accessFormFields: Field<string>[];
   formData: any = {};
@@ -52,13 +56,22 @@ export class RequestUserAccountsComponent implements OnInit {
   selectedOrgUnitItemsForDataEntry: any[] = [];
   selectedOrgUnitItemsForReport: any[] = [];
 
+  selectedRoles: any[] = [];
+  selectedUserGroups: any[] = [];
+
   shouldConfirm: boolean = false;
   saving: boolean = false;
 
   currentUserToCreateSelected: string;
   readyToSave: boolean = false;
+  pageReady: boolean = false;
+  formUpdateIsDone: boolean = false;
 
-  constructor() {}
+  constructor(
+    private dataStoreService: DataStoreDataService,
+    private messageAndDataStoreService: MessagesAndDatastoreService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     const storedUsersData = localStorage.getItem('usersToCreate');
@@ -77,6 +90,15 @@ export class RequestUserAccountsComponent implements OnInit {
         this.formDataToStoreLocally[
           this.formDataToStoreLocally?.length - 1
         ]?.id;
+      this.selectedRoles =
+        this.formDataToStoreLocally[
+          this.formDataToStoreLocally?.length - 1
+        ]?.userRoles;
+
+      this.selectedUserGroups =
+        this.formDataToStoreLocally[
+          this.formDataToStoreLocally?.length - 1
+        ]?.userGroups;
 
       console.log(
         'currentUserToCreateSelected',
@@ -94,6 +116,189 @@ export class RequestUserAccountsComponent implements OnInit {
         ? this.formDataToStoreLocally[this.formDataToStoreLocally?.length - 1]
         : null
     );
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
+
+  onNext(event: Event): void {
+    event.stopPropagation();
+    // Ensure the first ones have been taken and stored on localstorage
+    // Do not clear access control (only clear demographic)
+
+    // Check if you are editing existing item
+    this.pageReady = false;
+    this.formDataToStoreLocally = !this.formUpdateIsDone
+      ? this.formDataToStoreLocally
+      : !this.currentUserToCreateSelected
+      ? [
+          ...this.formDataToStoreLocally,
+          {
+            id: 'REF' + Date.now(),
+            firstName: this.formData?.firstName?.value,
+            middleName: this.formData?.middleName?.value,
+            lastName: this.formData?.lastName?.value,
+            phoneNumber: this.formData?.phoneNumber?.value,
+            email: this.formData?.email?.value,
+            title: this.formData?.title?.value,
+            titleDescription: this.formData?.titleDescription?.value,
+            entryOrgUnits: this.selectedOrgUnitItemsForDataEntry,
+            reportOrgUnits: this.selectedOrgUnitItemsForReport,
+            userGroups: this.selectedUserGroups,
+            userRoles: this.selectedRoles,
+          },
+        ]
+      : this.formDataToStoreLocally?.map((data) => {
+          if (data?.id === this.currentUserToCreateSelected) {
+            return {
+              id: this.currentUserToCreateSelected,
+              firstName: this.formData?.firstName?.value,
+              middleName: this.formData?.middleName?.value,
+              lastName: this.formData?.lastName?.value,
+              phoneNumber: this.formData?.phoneNumber?.value,
+              email: this.formData?.email?.value,
+              title: this.formData?.title?.value,
+              titleDescription: this.formData?.titleDescription?.value,
+              entryOrgUnits: this.formData?.entry?.value,
+              reportOrgUnits: this.formData?.report?.value,
+              userGroups: [
+                {
+                  id: this.formData?.userGroup?.value,
+                },
+              ],
+              userRoles: [
+                {
+                  id: this.formData?.userRole?.value,
+                },
+              ],
+            };
+          } else {
+            return data;
+          }
+        });
+    console.log(this.formData);
+    console.log(this.formDataToStoreLocally);
+    this.currentUserToCreateSelected = null;
+    localStorage.setItem(
+      'usersToCreate',
+      JSON.stringify(this.formDataToStoreLocally)
+    );
+    this.createDemographicFields();
+  }
+
+  onFinish(event: Event): void {
+    event.stopPropagation();
+    this.readyToSave = true;
+  }
+
+  onClear(event: Event): void {
+    event.stopPropagation();
+
+    this.createDemographicFields();
+    this.createAccessControlFields();
+  }
+
+  onSave(event: Event, confirmed?: boolean): void {
+    event.stopPropagation();
+    if (confirmed) {
+      this.saving = true;
+      this.shouldConfirm = false;
+      this.readyToSave = false;
+      // Clear local storage
+      // Send data to datastore and messaging after confirm
+      const dataStoreKey = 'UA' + Date.now();
+      const dataForMessageAndDataStore = {
+        id: dataStoreKey,
+        ticketNumber: 'UA' + Date.now().toString(),
+        action: `Respond to creation of ${this.formDataToStoreLocally?.length} accounts as requested by ${this.currentUser?.display}`,
+        message: {
+          message: `The following accounts are requested accordingly: \n\n ${this.formDataToStoreLocally?.map(
+            (data) => {
+              return (
+                'Names: <b>' +
+                data?.firstName +
+                ' ' +
+                data?.middleName +
+                ' ' +
+                data?.lastName +
+                ' </b>' +
+                ' Email: ' +
+                data?.email +
+                ' Phone number :' +
+                data?.phoneNumber +
+                ' Access level: Entry side -> ' +
+                data?.entryOrgUnits?.map((ou) => ou?.name).join(', ') +
+                ' and Report side ->' +
+                data?.reportOrgUnits?.map((ou) => ou?.name).join(', ')
+              );
+            }
+          )} `,
+          subject: 'UA' + Date.now().toString() + '- MAOMBI YA ACCOUNT',
+        },
+        replyMessage: 'to be constructed',
+        payload: this.formDataToStoreLocally?.map((data) => {
+          return {
+            userCredentials: {
+              cogsDimensionConstraints: [],
+              catDimensionConstraints: [],
+              username: '',
+              password: '',
+              userRoles: data?.userRoles,
+            },
+            surname: data?.middleName + ' ' + data?.lastName,
+            firstName: data?.firstName,
+            email: data?.email,
+            phoneNumber: data?.phoneNumber,
+            organisationUnits: data?.entryOrgUnits,
+            dataViewOrganisationUnits: data?.reportOrgUnits,
+            userGroups: data?.userGroups,
+            attributeValues: [],
+          };
+        }),
+        url: 'users',
+        method: 'POST',
+        user: this.currentUser,
+      };
+
+      const messageData = {
+        subject: dataForMessageAndDataStore?.message?.subject,
+        messageType: 'TICKET',
+        users: [],
+        userGroups: [{ id: this.systemConfigs?.feedbackRecipients?.id }],
+        organisationUnits: [],
+        attachments: [],
+        text: dataForMessageAndDataStore?.message?.message,
+      };
+
+      console.log('messageData', messageData);
+
+      this.messageAndDataStoreService
+        .createMessageAndUpdateDataStore(messageData, {
+          id: dataStoreKey,
+          user: {
+            id: this.currentUser?.id,
+            displayName: this.currentUser?.displayName,
+            userName: this.currentUser?.userCredentials?.username,
+            jobTitle: this.currentUser?.jobTitle,
+            email: this.currentUser?.email,
+            organisationUnits: this.currentUser?.organisationUnits,
+            phoneNumber: this.currentUser?.phoneNumber,
+          },
+          message: dataForMessageAndDataStore?.message,
+          ...dataForMessageAndDataStore,
+        })
+        .subscribe((response) => {
+          window.localStorage.removeItem('usersToCreate');
+          this.saving = false;
+          this.openSnackBar('Successfully sent form request', 'Close');
+          setTimeout(() => {
+            this._snackBar.dismiss();
+          }, 2000);
+        });
+    } else {
+      this.shouldConfirm = true;
+    }
   }
 
   createDemographicFields(data?: any): void {
@@ -160,6 +365,9 @@ export class RequestUserAccountsComponent implements OnInit {
         value: data ? data?.titleDescription : null,
       }),
     ];
+
+    console.log(this.formFields);
+    this.pageReady = true;
   }
 
   createAccessControlFields(data?: any): void {
@@ -199,139 +407,46 @@ export class RequestUserAccountsComponent implements OnInit {
         ),
       }),
     ];
+    this.pageReady = true;
+    console.log(this.accessFormFields);
   }
 
   onUpdateDemographicForm(formvalue: FormValue): void {
     this.formData = { ...this.formData, ...formvalue.getValues() };
-    console.log(formvalue.isValid);
+    this.formUpdateIsDone = true;
     this.isDemographicFormValid = formvalue.isValid;
   }
 
   onUpdateAccessControlForm(formvalue: FormValue): void {
     this.formData = { ...this.formData, ...formvalue.getValues() };
-    console.log(formvalue.isValid);
+    this.formUpdateIsDone = true;
+    this.selectedRoles = [
+      {
+        id: this.formData?.userRole?.id,
+      },
+    ];
+
+    this.selectedUserGroups = [
+      {
+        id: this.formData?.userGroup?.id,
+      },
+    ];
     this.isAccessControlFormValid = formvalue.isValid;
   }
 
   onOrgUnitUpdate(selection: any, type: string): void {
     if (type === 'ENTRY') {
+      this.selectedOrgUnitItemsForDataEntry = selection?.items;
       this.formData['entry'] = {
         id: 'ENTRY',
-        value: selection?.items,
+        value: this.selectedOrgUnitItemsForDataEntry,
       };
     } else {
+      this.selectedOrgUnitItemsForReport = selection?.items;
       this.formData['report'] = {
         id: 'REPORT',
-        value: selection?.items,
+        value: this.selectedOrgUnitItemsForReport,
       };
-    }
-  }
-
-  onNext(event: Event): void {
-    event.stopPropagation();
-    // Ensure the first ones have been taken and stored on localstorage
-    // Do not clear access control (only clear demographic)
-
-    // Check if you are editing existing item
-    this.formDataToStoreLocally = !this.currentUserToCreateSelected
-      ? [
-          ...this.formDataToStoreLocally,
-          {
-            id: 'REF' + Date.now(),
-            firstName: this.formData?.firstName?.value,
-            middleName: this.formData?.middleName?.value,
-            lastName: this.formData?.lastName?.value,
-            phoneNumber: this.formData?.phoneNumber?.value,
-            email: this.formData?.email?.value,
-            title: this.formData?.title?.value,
-            titleDescription: this.formData?.titleDescription?.value,
-            entryOrgUnits: this.formData?.entry?.value,
-            reportOrgUnits: this.formData?.report?.value,
-            userGroups: [
-              {
-                id: this.formData?.userGroup?.value,
-              },
-            ],
-            userRoles: [
-              {
-                id: this.formData?.userRole?.value,
-              },
-            ],
-          },
-        ]
-      : this.formDataToStoreLocally?.map((data) => {
-          if (data?.id === this.currentUserToCreateSelected) {
-            return {
-              id: this.currentUserToCreateSelected,
-              firstName: this.formData?.firstName?.value,
-              middleName: this.formData?.middleName?.value,
-              lastName: this.formData?.lastName?.value,
-              phoneNumber: this.formData?.phoneNumber?.value,
-              email: this.formData?.email?.value,
-              title: this.formData?.title?.value,
-              titleDescription: this.formData?.titleDescription?.value,
-              entryOrgUnits: this.formData?.entry?.value,
-              reportOrgUnits: this.formData?.report?.value,
-              userGroups: [
-                {
-                  id: this.formData?.userGroup?.value,
-                },
-              ],
-              userRoles: [
-                {
-                  id: this.formData?.userRole?.value,
-                },
-              ],
-            };
-          } else {
-            return data;
-          }
-        });
-
-    this.currentUserToCreateSelected = null;
-    localStorage.setItem(
-      'usersToCreate',
-      JSON.stringify(this.formDataToStoreLocally)
-    );
-    this.createDemographicFields();
-  }
-
-  onFinish(event: Event): void {
-    event.stopPropagation();
-    this.readyToSave = true;
-  }
-
-  onClear(event: Event): void {
-    event.stopPropagation();
-
-    this.createDemographicFields();
-    this.createAccessControlFields();
-  }
-
-  onSave(event: Event, confirmed?: boolean): void {
-    event.stopPropagation();
-    if (confirmed) {
-      this.saving = true;
-      this.shouldConfirm = false;
-      this.readyToSave = false;
-      // Clear local storage
-      // Send data to datastore and messaging after confirm
-      const dataStructure = {
-        id: 'UA' + Date.now().toString(),
-        ticketNumber: 'UA' + Date.now().toString(),
-        action: 'Should contain action message',
-        message: {
-          message: 'message to DHIS2 messaging',
-          subject: 'UA' + Date.now().toString() + '- MAOMBI YA ACCOUNT',
-        },
-        replyMessage: '',
-        payload: {},
-        url: '',
-        method: 'POST',
-        user: this.currentUser,
-      };
-    } else {
-      this.shouldConfirm = true;
     }
   }
 }
--- src/app/pages/user-accounts/pages/user-accounts-request-dashboard/user-accounts-request-dashboard.component.ts
+++ src/app/pages/user-accounts/pages/user-accounts-request-dashboard/user-accounts-request-dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/internal/Observable';
 import { DataStoreDataService } from 'src/app/core/services/datastore.service';
+import { State } from 'src/app/store/reducers';
+import { getCurrentUser } from 'src/app/store/selectors';
+import { getSystemConfigs } from 'src/app/store/selectors/system-configurations.selectors';
 
 @Component({
   selector: 'app-user-accounts-request-dashboard',
@@ -9,10 +13,17 @@ import { DataStoreDataService } from 'src/app/core/services/datastore.service';
 })
 export class UserAccountsRequestDashboardComponent implements OnInit {
   configurations$: Observable<any>;
-  constructor(private dataStoreDataService: DataStoreDataService) {}
+  currentUser$: Observable<any>;
+  systemConfigs$: Observable<any>;
+  constructor(
+    private dataStoreDataService: DataStoreDataService,
+    private store: Store<State>
+  ) {}
 
   ngOnInit(): void {
     this.configurations$ =
       this.dataStoreDataService.getUserSupportConfigurations();
+    this.currentUser$ = this.store.select(getCurrentUser);
+    this.systemConfigs$ = this.store.select(getSystemConfigs);
   }
 }
