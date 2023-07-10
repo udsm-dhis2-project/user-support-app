f2bc24c4dd665b081cf1c69af9f38a7b73da16d3
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-07-10 07:40:22 +0300

    feat: add support to list requested validation rules

--- src/app/pages/validation-rules/components/requested-validation-rules/requested-validation-rules.component.ts
+++ src/app/pages/validation-rules/components/requested-validation-rules/requested-validation-rules.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { DataStoreDataService } from 'src/app/core/services/datastore.service';
 
 @Component({
   selector: 'app-requested-validation-rules',
   templateUrl: './requested-validation-rules.component.html',
-  styleUrls: ['./requested-validation-rules.component.css']
+  styleUrls: ['./requested-validation-rules.component.css'],
 })
 export class RequestedValidationRulesComponent implements OnInit {
-
-  constructor() { }
+  @Input() configurations: any;
+  validationRulesRequests$: Observable<any[]>;
+  detailedRequests: any = {};
+  constructor(
+    private dataStoreService: DataStoreDataService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
+    this.validationRulesRequests$ = this.dataStoreService.getAllFromNameSpace(
+      'dataStore/dhis2-user-support',
+      {
+        ...this.configurations,
+        category: 'VR',
+      }
+    );
   }
 
+  onViewDetails(event: Event, validationRuleRequest: any): void {
+    event.stopPropagation();
+    this.detailedRequests[validationRuleRequest?.id] = validationRuleRequest;
+  }
 }
