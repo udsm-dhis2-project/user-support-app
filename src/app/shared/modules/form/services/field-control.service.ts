672a20fdc132ce3125912ffdc3d8bcf646758934
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-03-11 12:14:23 +0300

    feat: add support to validate email and partial phone number validation

--- src/app/shared/modules/form/services/field-control.service.ts
+++ src/app/shared/modules/form/services/field-control.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from "@angular/core";
-import { UntypedFormControl, UntypedFormGroup, Validators } from "@angular/forms";
-import { Field } from "../models/field.model";
-import { FieldsData } from "../models/fields-data.model";
+import { Injectable } from '@angular/core';
+import {
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Field } from '../models/field.model';
+import { FieldsData } from '../models/fields-data.model';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class FieldControlService {
   constructor() {}
 
-  toFormGroup(fields: Field<string>[], fieldsData?: FieldsData): UntypedFormGroup {
+  toFormGroup(
+    fields: Field<string>[],
+    fieldsData?: FieldsData
+  ): UntypedFormGroup {
     const group: any = {};
     fields.forEach((field) => {
       const fieldData = fieldsData ? fieldsData[field.id]?.latest : null;
@@ -15,15 +22,41 @@ export class FieldControlService {
         group[field.key] = field.required
           ? new UntypedFormControl(
               {
-                value: fieldData?.value || field.value || "",
+                value: fieldData?.value || field.value || '',
                 disabled: field?.disabled,
               },
-              Validators.required
+              [
+                Validators.required,
+                field?.type === 'phonenumber' ? Validators.minLength(10) : null,
+                field?.type === 'phonenumber' ? Validators.maxLength(10) : null,
+                field?.type === 'phonenumber'
+                  ? Validators.pattern(
+                      /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+                    )
+                  : null,
+                field?.type === 'email'
+                  ? Validators.pattern(/(.+)@(.+){2,}\.(.+){2,}/)
+                  : null,
+              ].filter((validator) => validator)
             )
-          : new UntypedFormControl({
-              value: fieldData?.value || field.value || "",
-              disabled: field?.disabled,
-            });
+          : new UntypedFormControl(
+              {
+                value: fieldData?.value || field.value || '',
+                disabled: field?.disabled,
+              },
+              [
+                field?.type === 'phonenumber' ? Validators.minLength(10) : null,
+                field?.type === 'phonenumber' ? Validators.maxLength(10) : null,
+                field?.type === 'phonenumber'
+                  ? Validators.pattern(
+                      /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+                    )
+                  : null,
+                field?.type === 'email'
+                  ? Validators.pattern(/(.+)@(.+){2,}\.(.+){2,}/)
+                  : null,
+              ].filter((validator) => validator)
+            );
       }
     });
 
